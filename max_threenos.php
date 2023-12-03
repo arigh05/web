7c069e7f5754b49122fd00f113394d56badfb5af
Ari <135527443+arigh05@users.noreply.github.com>
2023-12-03 18:16:46 +0530

    Add files via upload

---
+++ max_threenos.php
@@ -0,0 +1,7 @@
+<?php
+  $num1 = readline("Enter the 1st number: ");
+  $num2 = readline("Enter the 2nd number: ");
+  $num3 = readline("Enter the 3rd number: ");
+  echo "Greatest among $num1 $num2 $num3 is " . max($num1, $num2, $num3);
+ ?>
+ 
